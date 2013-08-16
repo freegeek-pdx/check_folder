fd02dfe2e0fb0797a18de96172a659256de36889
paulm <paulm@freegeek.org>
2013-08-16 12:09:01 -0700

    mpved to new repo

--- dell-fix-prep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get remove linux 855.gm-fix.dkms
-sudo rm /etc/apt/sources.list.d/glasen-*
-sudo rm /etc/apt/sources.list.d/brian-*
-sudo rm /etc/X11/xorg.conf
-apt-get update
-apt-get upgrade
